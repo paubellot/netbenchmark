fe736e49f3702c9bf7e9ca8dc6ca3b38d85224bd
Pau Bellot <pau.bellot@upc.edu>
2015-05-15 08:35:31 +0000

    - Add routine to unregister a wrapper
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/netbenchmark@103861 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/RegistrerWrapper.R
+++ R/RegistrerWrapper.R
@@ -5,12 +5,36 @@ RegistrerWrapper<-function(wrapper.name=NULL,all.fast=TRUE){
             Fast<-get("Fast", ntb_globals)
             assign("Fast", c(Fast,wrapper.name),  ntb_globals)
             message(paste(wrapper.name, "added to all.fast methods"))
+            message("The methods registered in all.fast are:")
+            message(c(Fast,wrapper.name))
         }else{
             All<-get("All",ntb_globals)
             assign("All",c(All,wrapper.name),  ntb_globals)
             message(paste(wrapper.name, "added to all methods"))
+            message("The methods registered in all are:")
+            message(c(All,wrapper.name))
         }
     }else{
         stop(paste(wrapper.name,"does not exist!"))
+    }
+}
+
+UnregistrerWrapper<-function(wrapper.name=NULL,all.fast=TRUE){
+    if(all.fast){
+        Fast <- get("Fast", ntb_globals)
+        idx <- which(wrapper.name==Fast)
+        if(idx>0){
+            Fast<-Fast[-idx]
+            assign("Fast", c(Fast,wrapper.name),  ntb_globals)
+            message(paste(wrapper.name, "removed from all.fast methods"))
+        }
+    }else{
+        All <- get("All", ntb_globals)
+        idx <- which(wrapper.name==All)
+        if(idx>0){
+            All<-All[-idx]
+            assign("All", c(All,wrapper.name),  ntb_globals)
+            message(paste(wrapper.name, "removed from all methods"))
+        }
     }
 }
