3033a39dbfba3bc82db1bd83cf4807a8e0a444ea
Pau Bellot <pau.bellot@upc.edu>
2015-08-08 09:38:02 +0000

    - Bugfix GeneNet wrapper - Documentation enhancements
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/netbenchmark@107250 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GeneNet.wrap.R
+++ R/GeneNet.wrap.R
@@ -2,10 +2,20 @@ GeneNet.wrap <- function(data){
     # method <- "static"
     # p <- 0.8
     ngenes <- dim(data)[2]
-    pcor <- ggm.estimate.pcor(data,method ="static",verbose=FALSE)
-    test.results <- network.test.edges(pcor,plot=FALSE,verbose=FALSE)
-    idx <- which(test.results$prob > 0.8)
-    aux<-network.make.graph(test.results[idx,],node.labels = colnames(data))
-    net<-as(aux,"matrix")
-    return(net);
+    pcor <- GeneNet::ggm.estimate.pcor(t(data),
+                                       method ="static",verbose=FALSE)
+    C <- fdrtool::fdrtool(corpcor::sm2vec(pcor),plot=FALSE,
+                          statistic="correlation",verbose=FALSE)$param
+    test.results <- GeneNet::network.test.edges(pcor,plot=FALSE,verbose=FALSE)
+    crop <- GeneNet::extract.network(test.results,cutoff.ggm=C[1,"cutoff"],
+                                     verbose=FALSE)
+    if(dim(crop)[1]==0){
+        d <- round(dim(test.results)[1]/10)
+        crop <- GeneNet::extract.network(test.results, 
+                                         method.ggm="number", cutoff.ggm=d,
+                                         verbose=FALSE)
+    }
+    aux <- GeneNet::network.make.graph(crop,node.labels = colnames(data))
+    net <- as(aux,"matrix")
+    return(net)
 }
